8b47b3268ed07eaceee7c1e5774b4c60441242dc
prabhakarsingh2007 <prabhakarkumarsingh578@gmail.com>
2026-07-06 09:11:47 +0530

    feat: initialize Django project settings with production environment configurations and WhiteNoise integration

--- expense_tracker/expense_tracker/settings.py
+++ expense_tracker/expense_tracker/settings.py
@@ -5,6 +5,7 @@ Django settings for expense_tracker project.
 from pathlib import Path
 import os
 from decouple import config
+import dj_database_url
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
